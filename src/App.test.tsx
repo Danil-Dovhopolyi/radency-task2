0726814cbe8e2c6b8e6cf339d0c450ae670a4a0f
DanilDovhopolyi <ddo2102@gmail.com>
2023-07-30 01:16:26 +0300

    Setup project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { describe, it } from 'vitest';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { App } from './App';
+
+describe('App', () => {
+  it('should render', () => {
+    expect(App).toBeDefined();
+  });
 });
